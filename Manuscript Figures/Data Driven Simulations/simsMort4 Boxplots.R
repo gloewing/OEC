f89bd110e53f08c43aa0927d891855be62929b55
gloewing <40547905+gloewing@users.noreply.github.com>
2021-05-24 20:06:36 -0400

    Add files via upload

--- Manuscript Figures/Data Driven Simulations/simsMort4 Boxplots.R
+++ Manuscript Figures/Data Driven Simulations/simsMort4 Boxplots.R
@@ -1,10 +1,9 @@
-# test whether annealing from right or left is better
 # generalist specialist, zero out
 ######################################################
 # compare different K and betaVar -- no regularization 
 ######################################################
-# setwd("~/Desktop/Research/simsMort4")
-setwd("~/Desktop/OEC/Manuscript Figures/Data Driven Simulations/simsMort4")
+setwd("~/Desktop/Research/simsMort4")
+# setwd("~/Desktop/OEC/Manuscript Figures/Data Driven Simulations/simsMort4")
 library(latex2exp)
 library(tidyverse)
 library(ggplot2)
@@ -22,37 +21,37 @@ itrs <- 100 # number of simulation iterations
 cnt <- 0
 rmseMat <- matrix(nc = length(bVar), nr = itrs)
 for(tn in tune){
-for(yrs in year){
+    for(yrs in year){
         for(b in 1:length(bVar)){
             for(k in 1:length(Kvec)){
                 cnt <- cnt + 1 # counter
                 K <- Kvec[k]
                 x <- bVar[b]
-                                
+                
                 flNm <- paste0("oecMort_0.5.etTn_TRUE_cv_stTn_cv_oecTn_", tn, "_stTnIn_TRUE_stCV_cv_etaSpc_TRUE_Wcv_cv_Wspc_FALSE_sclX_TRUE_glm_FALSE_sampSz_NA_smpSzW_1_tstTrn_52_minTrn_100_bVar_", x, "_reTn_FALSE_eta_TRUE_yr_",yrs,"_K_",K)
                 
                 if(file.exists(flNm)){
                     # check to see if file exists
                     d <- read.csv(flNm) 
                     
-                        rmseMat <- data.frame(Generalist = d$oec_test2 / d$stack2,
-                                              Specialist = d$oec_country2 / d$stacking_country,
-                                              ZeroOut = d$oec_country0 / d$stacking_country_zeroOut
-                        )
-                        
-                        ls_beta[[cnt]] <- cbind( 
-                            gather(rmseMat), 
-                            K, 
-                            x,
-                            yrs,
-                            tn
-                        )
-         
-    }
-}
-
+                    rmseMat <- data.frame(Generalist = d$oec_test2 / d$stack2,
+                                          Specialist = d$oec_country2 / d$stacking_country,
+                                          ZeroOut = d$oec_country0 / d$stacking_country_zeroOut
+                    )
+                    
+                    ls_beta[[cnt]] <- cbind( 
+                        gather(rmseMat), 
+                        K, 
+                        x,
+                        yrs,
+                        tn
+                    )
+                    
+                }
+            }
+            
         }
-}
+    }
 }
 #############
 # process data
@@ -68,8 +67,7 @@ dat$tn <- as.factor(dat$tn)
 #############
 meths <- c("Specialist", "ZeroOut")
 tune <- c("zero", "cvCF") 
-t = "zero"
-meths = "Specialist"
+
 for(t in tune){
     for(m in meths){
         
@@ -91,44 +89,17 @@ for(t in tune){
             scale_color_manual(values = RColorBrewer::brewer.pal(5, "Reds")) +
             labs(x = TeX('$\\mathbf{\\sigma^2_{\\beta}}}$'),
                  y = TeX('$\\mathbf{RMSE_{OEC}/RMSE_{Stack}}$'),
-                 color = "Number of \nstudies",
-                 fill  = "Number of \nstudies") +
-            theme_bw()
-        # dat %>% 
-        #     tibble %>% 
-        #     dplyr::filter(key == m)  %>% 
-        #     dplyr::filter(tn == t) %>%
-        #     ggplot( aes( y = value, x = x, fill = K )) + 
-        #     geom_boxplot(
-        #         lwd = 1.5, 
-        #         fatten = 0.5, 
-        #         alpha = 0.5 
-        #     ) +
-        #     theme_classic(base_size = 12) +
-        #     theme( plot.title = element_text(hjust = 0.5, color="black", size=rel(2), face="bold"),
-        #            axis.text=element_text(face="bold",color="black", size=rel(1.75)),
-        #            axis.title = element_text(face="bold", color="black", size=rel(1.5)),
-        #            legend.key.size = unit(2, "line"), # added in to increase size
-        #            legend.text = element_text(face="bold", color="black", size = rel(1.75)), # 3 GCL
-        #            legend.title = element_text(face="bold", color="black", size = rel(2))
-        #     ) +
-        #     geom_hline(yintercept=1, 
-        #                linetype="dashed", 
-        #                color = "black", 
-        #                size = rel(0.5),
-        #                alpha = 0.7) + #
-        #     ylim(0.5, 1.5) +
-        #     ylab(TeX('$\\mathbf{RMSE_{OEC}/RMSE_{Stacking}}$') )+ 
-        #     xlab(TeX('$\\mathbf{\\sigma^2_{\\beta}}}$')) + 
-        #     scale_color_manual(values = c("red", "blue", "green", "#0868ac", "#E69F00", "#525252")) + # ,
-        #     scale_fill_manual(values = c("red", "blue", "green", "#0868ac", "#E69F00","#525252"))  # ,
-        
+                 color = "Studies (K)",
+                 fill  = "Studies (K)") +
+            theme_bw() + 
+            theme(text = element_text(face = "bold"))
+
         # save figures
         setwd("~/Desktop/Research Final/Mortality/Figures/Final Figures/Data-Driven Simulations/simsMort4 Figures")
-        ggsave(paste0("mortSims_", m, "_", tn, ".pdf"),
+        ggsave(paste0("mortSims_", m, "_", t, ".pdf"),
                plot   = plt1,
                dpi    = 300,
-               width  = 5, 
-               height = 3)
+               width  = 6, 
+               height = 4)
     }
 }
